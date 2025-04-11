ea8e0c646b05ff115ebb3068a132090067163816
solitude <2846037730@qq.com>
2025-04-11 18:43:50 +0800

    hw04

--- HW/hw04/hw04.py
+++ HW/hw04/hw04.py
@@ -13,6 +13,14 @@ def shuffle(s):
     """
     assert len(s) % 2 == 0, 'len(seq) must be even'
     "*** YOUR CODE HERE ***"
+    half = len(s)//2
+    s0=s[:half]
+    s1=s[half:]
+    ans=[]
+    for i in range(half): 
+        ans+=[s0[i]]
+        ans+=[s1[i]]
+    return ans
 
 
 def deep_map(f, s):
@@ -38,6 +46,11 @@ def deep_map(f, s):
     True
     """
     "*** YOUR CODE HERE ***"
+    for i in range(len(s)):
+        if type(s[i])==list:
+            deep_map(f,s[i])
+        else:
+            s[i]=f(s[i])
 
 
 HW_SOURCE_FILE=__file__
@@ -47,11 +60,13 @@ def planet(mass):
     """Construct a planet of some mass."""
     assert mass > 0
     "*** YOUR CODE HERE ***"
+    return ['planet', mass]
 
 def mass(p):
     """Select the mass of a planet."""
     assert is_planet(p), 'must call mass on a planet'
     "*** YOUR CODE HERE ***"
+    return p[1]
 
 def is_planet(p):
     """Whether p is a planet."""
@@ -104,6 +119,13 @@ def balanced(m):
     True
     """
     "*** YOUR CODE HERE ***"
+    if is_planet(m):
+        return True
+    else:
+        left_end, right_end = end(left(m)), end(right(m))
+        torque_left = length(left(m)) * total_mass(left_end)
+        torque_right = length(right(m)) * total_mass(right_end)
+        return torque_left == torque_right and balanced(left_end) and balanced(right_end)
 
 
 def berry_finder(t):
@@ -124,6 +146,12 @@ def berry_finder(t):
     True
     """
     "*** YOUR CODE HERE ***"
+    if label(t) == 'berry':
+        return True
+    for b in branches(t):
+        if berry_finder(b):
+            return True
+    return False
 
 
 HW_SOURCE_FILE=__file__
@@ -139,6 +167,10 @@ def max_path_sum(t):
     17
     """
     "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+      return label(t)
+    else:
+      return label(t) + max([max_path_sum(b) for b in branches(t)])
 
 
 def mobile(left, right):
